3c5bb4d8ed4de5b0f4f3062aee5db05332269daf
Ana Martínez <amartinezaguilar6@gmail.com>
2019-01-06 20:30:06 +0100

    Implement GET and POST

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 const dbName = 'books';
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
 app.get('/books', (req, res) => {
 
 // Connect using MongoClient
@@ -19,6 +26,27 @@ app.get('/books', (req, res) => {
 
         }
     });
+
+});
+
+app.post('/books', (req, res) => {
+
+    if(req.body.title && req.body.author){
+
+        MongoClient.connect(url, function(err, client) {
+            if(!err){
+                const col = client.db(dbName).collection('books');
+                col.insertOne(req.body, (err, result) => {
+                    if(!err){
+                        res.send(req.body);
+                    }
+                });
+            }
+        });
+
+    } else {
+        res.send('Error: You must send at least title and author');
+    }
 
 });
 
